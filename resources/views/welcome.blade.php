36a11ef3bf1d53ac91de8db234f7e222b4d8f7d8
TaikiHattori <tvajarvis@outlook.jp>
2025-05-11 10:41:50 +0900

    first commit pushできるかチェック

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+    </head>
+    <body>
+    <div style="text-align: center; margin-top: 50px;">
+        <h1>Docker Icon App</h1>
+        <button>クリック</button>
+    </div>
+</body>
+</html>
